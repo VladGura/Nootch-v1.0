e03eec02bb120c439d3d82504ae168e04c8e59ae
VladGura <vlad.gura@yahoo.com>
2024-02-04 23:28:24 +0200

    Setup for Spring Boot

--- src/com/nootch/Main.java
+++ src/com/nootch/Main.java
@@ -1,8 +1,22 @@
 package com.nootch;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+class Nootch {
 
     public static void main(String[] args) {
-	// write your code here
+        SpringApplication.run(Nootch.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
     }
 }

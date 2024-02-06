812fc2e3b2e79d9174d4423608e562e49489c69a
Erena <markerena2005@gmail.com>
2024-02-06 23:48:39 +0200

    Added error handling and organized code

---
+++ src/main/java/com/nootch/ErrorHandlerController.java
@@ -0,0 +1,19 @@
+package com.nootch;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class ErrorHandlerController implements ErrorController {
+
+    @RequestMapping("/error")
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public RedirectView error() {
+        return new RedirectView("/error.html");
+    }
+
+}
--- src/main/java/com/nootch/Main.java
+++ src/main/java/com/nootch/Main.java
@@ -1,9 +1,9 @@
 package com.nootch;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -12,6 +12,7 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 class Nootch {
 
     public static void main(String[] args) {
@@ -21,28 +22,5 @@ class Nootch {
     @GetMapping("/hello")
     public List<String> hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return List.of("Hello", name);
-    }
-
-    @RequestMapping(value = "/register",
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public RedirectView register(@RequestParam(name = "username") String username, @RequestParam(name = "email") String email, @RequestParam(name = "password") String password) {
-        //add to database
-        return new RedirectView("/");
-    }
-
-    @RequestMapping(value = "/home",
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody String login(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password) {
-        System.out.println(username + " " + password.hashCode());
-        return "Boohoo n-word";
-    }
-
-    @GetMapping("/home")
-    public RedirectView homeError() {
-        return new RedirectView("/");
     }
 }
